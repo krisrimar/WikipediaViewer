332d58d178720286c76c64badf0e577496809b1e
Kris Rimar <krisrimar@Kris-Rimar-MacBook-Pro.local>
2017-06-18 11:18:21 +0300

    Add random Wiki page button, add crop text function, code clean-up
    - Clicking the button takes the user to a random Wikipedia page
    - Add crop text function to crop only the text that is more than set
    amount of chars in length (only concerns “extract” text)
    - Change title of page to “WikiSearch”

--- wiki.js
+++ wiki.js
@@ -1,17 +1,14 @@
 
-//DEBUG
+//CONSTANTS
 
 const DEBUG = false;
+const MAX_ALLOWED_TEXT_LENGTH = 320;
 
 //LINKS
 
 const wikipediaSearchAPILink ="http://en.wikipedia.org/w/api.php?action=query&generator=search&format=json&prop=info|extracts|pageterms&exintro=true&exchars=320&inprop=url&gsrsearch=";
 
-
 //TEXT
-
-// const NO_LOCATION_TEXT = "Could not determine your location";
-
 
 //UI ELEMENT IDs
 
@@ -21,8 +18,14 @@ const searchTextID = "#search-text";
 
 //VARIABLES
 
-
 //CODE
+
+function crop(text) {
+  if(text.length > MAX_ALLOWED_TEXT_LENGTH) {
+    text = text.substr(0,MAX_ALLOWED_TEXT_LENGTH) + "...";
+  }
+  return text;
+}
 
 function displaySearchResults(data) {
 
@@ -30,9 +33,13 @@ function displaySearchResults(data) {
 
   var searchResultsArray = data.query.pages;
 
+  //TO DO: handle error when user searches an empty string
+
   for(var key in searchResultsArray) {
+
     if(DEBUG) { console.log(searchResultsArray[key].title); };
-    $(searchResultsID).append("<a href=" + searchResultsArray[key].fullurl + "><div class='card' style='padding:10px;margin-bottom:10px'><h4>" + searchResultsArray[key].title + "</h4><p>" + (searchResultsArray[key].extract).substr(0,320) + "...</p></div></a>");
+
+    $(searchResultsID).append("<a href=" + searchResultsArray[key].fullurl + "><div class='card' style='padding:10px;margin-bottom:10px'><h4>" + searchResultsArray[key].title + "</h4><p>" + crop(searchResultsArray[key].extract) + "</p></div></a>");
   }
 }
 
@@ -47,10 +54,12 @@ function makeRequest(searchText) {
     type: 'POST',
     headers: { 'Api-User-Agent': 'KrisRimarWiki/1.1 (rimarkris@gmail.com)'},
     success: function(data) {
+
       if(DEBUG) { console.log("Value of search: " + searchText); };
       if(DEBUG) { console.log("Received results of AJAX request"); };
 
       displaySearchResults(data);
+
     },
     error: function(error) {
       if(DEBUG) { console.log("no search results"); };
@@ -63,11 +72,15 @@ function makeRequest(searchText) {
 
 $(document).ready(function(){
   if(DEBUG) { makeRequest("hell"); };
+
+  //Make request when user is on search field an presses "Enter" button
   $(searchTextID).keyup(function(event){
     if(event.keyCode == 13){
       makeRequest($(searchTextID).val());
     }
   });
+
+  //Make request when user presses "Seach" button on the UI
   $(searchWikiButtonID).on('click', function(e) {
     makeRequest($(searchTextID).val());
   });
