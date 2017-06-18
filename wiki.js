ce93ebcce998806c7d1fe61be7b706436216f9bb
Kris Rimar <krisrimar@Kris-Rimar-MacBook-Pro.local>
2017-06-18 10:57:58 +0300

    Update expected model from Wikipedia, update Search UI
    - Change URL of Wiki API
    - Add “extract” text to search results
    - Re-style search field
    - Add ability to search by pressing “Enter” key

--- wiki.js
+++ wiki.js
@@ -1,11 +1,11 @@
 
-const DEBUG = true;
+//DEBUG
+
+const DEBUG = false;
 
 //LINKS
 
-const wikipediaSearchAPILink = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&inprop=url&srsearch=";
-
-//const wikipediaSearchAPILink ="http://en.wikipedia.org/w/api.php?action=query&generator=search&format=json&gsrprop=snippet&prop=info&inprop=url&&gsrsearch=";
+const wikipediaSearchAPILink ="http://en.wikipedia.org/w/api.php?action=query&generator=search&format=json&prop=info|extracts|pageterms&exintro=true&exchars=320&inprop=url&gsrsearch=";
 
 
 //TEXT
@@ -25,49 +25,50 @@ const searchTextID = "#search-text";
 //CODE
 
 function displaySearchResults(data) {
-  //if(DEBUG) { console.log("This is the value of serach I got: " + searchText); };
+
   if(DEBUG) { console.log("Displaying results"); };
 
-  var searchResultsAmount = data.query.search.length;
-  var searchResultsArray = data.query.search;
+  var searchResultsArray = data.query.pages;
 
-  if(DEBUG) { console.log(searchResultsAmount); };
-
-  for(i = 0; i < searchResultsAmount; i += 1) {
-    if(DEBUG) { console.log("Result " + i + ": " + searchResultsArray[i].title); };
-    $(searchResultsID).append("<div class='card' style='padding:10px;margin-botton:10px'><h4>" + searchResultsArray[i].title + "</h4><p>" + searchResultsArray[i].snippet + "</p></div>");
+  for(var key in searchResultsArray) {
+    if(DEBUG) { console.log(searchResultsArray[key].title); };
+    $(searchResultsID).append("<a href=" + searchResultsArray[key].fullurl + "><div class='card' style='padding:10px;margin-bottom:10px'><h4>" + searchResultsArray[key].title + "</h4><p>" + (searchResultsArray[key].extract).substr(0,320) + "...</p></div></a>");
   }
 }
 
 
 function makeRequest(searchText) {
-$.ajax({
-  type: "GET",
-  url: wikipediaSearchAPILink + searchText,
-  data: searchText,
-  dataType: 'json',
-  type: 'POST',
-  headers: { 'Api-User-Agent': 'KrisRimarWiki/1.1 (rimarkris@gmail.com)'},
-  success: function(data) {
-    if(DEBUG) { console.log("Value of search: " + searchText); };
-    if(DEBUG) { console.log("Received results of AJAX request"); };
-    displaySearchResults(data);
-  },
-  error: function(error) {
-    if(DEBUG) { console.log("no search results"); };
-  },
-  cache: false
-});
+  $(searchResultsID).empty();
+  $.ajax({
+    type: "GET",
+    url: wikipediaSearchAPILink + searchText,
+    data: searchText,
+    dataType: 'json',
+    type: 'POST',
+    headers: { 'Api-User-Agent': 'KrisRimarWiki/1.1 (rimarkris@gmail.com)'},
+    success: function(data) {
+      if(DEBUG) { console.log("Value of search: " + searchText); };
+      if(DEBUG) { console.log("Received results of AJAX request"); };
+
+      displaySearchResults(data);
+    },
+    error: function(error) {
+      if(DEBUG) { console.log("no search results"); };
+    },
+    cache: false
+  });
 }
 
 //MAIN
 
 $(document).ready(function(){
+  if(DEBUG) { makeRequest("hell"); };
+  $(searchTextID).keyup(function(event){
+    if(event.keyCode == 13){
+      makeRequest($(searchTextID).val());
+    }
+  });
   $(searchWikiButtonID).on('click', function(e) {
-    $(searchResultsID).empty();
-    var searchText = $(searchTextID).val();
-    if(DEBUG) { console.log("Value of search field: " + $(searchTextID).val()); };
-
-    makeRequest(searchText);
+    makeRequest($(searchTextID).val());
   });
 });
